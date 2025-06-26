ab03d8012aff51f5b2f17e25a364b43211715aa9
songli <18601038090@163.com>
2025-06-26 22:20:37 +0800

    clean code

--- imind_ai/base_agent/graph.py
+++ imind_ai/base_agent/graph.py
@@ -1,64 +1,30 @@
-import asyncio
 from collections import deque
 from typing import (
-    Any,
     Callable,
-    Literal,
     Optional,
     Sequence,
     Type,
-    TypedDict,
     Union,
     Tuple,
 )
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from langchain_core.messages import AnyMessage, HumanMessage
 from langgraph.graph import StateGraph, START, MessagesState, END
 from langgraph.graph.graph import CompiledGraph
-from langchain.chat_models import init_chat_model
 from langchain.chat_models.base import BaseChatModel, _ConfigurableModel
-from langchain_core.embeddings import Embeddings
 from langchain_core.tools import BaseTool
 from langgraph.types import Checkpointer
 from langgraph.store.base import BaseStore
 from langgraph.prebuilt import ToolNode
-from langgraph.checkpoint.memory import InMemorySaver
-from langmem.short_term import SummarizationNode, RunningSummary
-from langchain_core.tools import tool
-
-
-class WeatherResponse(BaseModel):
-    """Respond to the user with this"""
-
-    temperature: float = Field(description="The temperature in fahrenheit")
-    wind_directon: str = Field(
-        description="The direction of the wind in abbreviated form"
-    )
-    wind_speed: float = Field(description="The speed of the wind in km/h")
-
-
-@tool
-def get_weather(city: Literal["nyc", "sf"]):
-    """Use this to get weather information."""
-    if city == "nyc":
-        return "It is cloudy in NYC, with 5 mph winds in the North-East direction and a temperature of 70 degrees"
-    elif city == "sf":
-        return "It is 75 degrees and sunny in SF, with 3 mph winds in the South-East direction"
-    else:
-        raise AssertionError("Unknown city")
+from langmem.short_term import SummarizationNode
 
 
 class State(MessagesState):
     user_input: Union[str, Tuple[str, dict]]
     summarized_messages: list[AnyMessage]
     llm_output: Union[str, BaseModel]
-
-
-# class LLMInputState(TypedDict):
-#     summarized_messages: list[AnyMessage]
-#     context: dict[str, Any]
 
 
 def create_base_agent(
@@ -141,13 +107,11 @@ def create_base_agent(
             )
         else:
             response = state["messages"][-1]
-        # We return the final answer
         return {"llm_output": response}
 
     def should_continue(state: State):
         messages = state["messages"]
         last_message = messages[-1]
-        print("last_message", last_message)
 
         if not last_message.tool_calls:
             return "post_processor"
@@ -170,48 +134,3 @@ def create_base_agent(
     graph = builder.compile(checkpointer=checkpointer, store=store, debug=debug)
 
     return graph
-
-
-tools = [get_weather]
-
-config = {"configurable": {"thread_id": "1"}}
-
-llm = init_chat_model("ollama:qwen2.5:14b")
-
-checkpointer = InMemorySaver()
-
-agent = create_base_agent(
-    llm, tools=tools, checkpointer=checkpointer, output_schema=WeatherResponse
-)
-
-
-async def main():
-    # resp = agent.invoke({"messages": "hi, i am bob"}, config)
-    # # print("\n\n\n")
-    # # print("resp1", resp)
-    # resp = agent.invoke({"messages": "what's the weather in nyc this weekend"}, config)
-    # # print("\n\n\n")
-    # # print("resp2", resp)
-    # resp = agent.invoke({"messages": "what's new on broadway?"}, config)
-    # print("\n\n\n")
-    # print("resp3", resp)
-
-    answer = await agent.ainvoke(
-        input={"user_input": "what's the weather in SF?"},
-        config=config,
-        # input={"messages": [("human", "你是谁？")]},
-        # config=config,
-    )
-
-    # print(answer)
-    for message in answer["messages"]:
-        message.pretty_print()
-    print("\n\n\n")
-    for message in answer["summarized_messages"]:
-        message.pretty_print()
-    print("\n\n\n")
-    print(answer["llm_output"])
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
